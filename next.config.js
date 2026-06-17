39ff5e40ef19b8f80e5e70c38a2507d5220343d0
hellj504-afk <hellj504@gmail.com>
2026-06-17 17:27:25 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: false,
+  images: {
+    unoptimized: true,
+  },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
