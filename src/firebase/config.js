eb4f14c86e11bd0de1a11b87c0575b03445e64ae
hellj504-afk <hellj504@gmail.com>
2026-06-17 17:04:40 +0500

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,7 @@
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBH4lFLNHsxVPk2Ripx9t4XqRrO6i6bK34",
   authDomain: "production-5766f.firebaseapp.com",
